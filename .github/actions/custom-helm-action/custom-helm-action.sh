c76a6ae6d7b5ccff8cb2d3c7e2acfcbf9a6709a1
alifarhat-eurus <ali.farhat@eurustechnologies.com>
2023-12-01 16:02:27 +0500

    version56

--- .github/actions/custom-helm-action/custom-helm-action.sh
+++ .github/actions/custom-helm-action/custom-helm-action.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+app=$1
 cd helm/helmfile.d
 wget https://github.com/helmfile/helmfile/releases/download/v0.159.0/helmfile_0.159.0_linux_amd64.tar.gz
 tar -xzvf helmfile_0.159.0_linux_amd64.tar.gz
@@ -6,7 +7,7 @@ tar -xzvf helmfile_0.159.0_linux_amd64.tar.gz
 
 ./helmfile init --force
 
-set +e ./helmfile diff --environment lower -f $1 --detailed-exitcode
+set +e ./helmfile diff --environment lower -f $app --detailed-exitcode
 exit_code=#$(echo $?)
 echo "----exit_code-----"
 echo $exit_code
@@ -15,27 +16,27 @@ if [[ $exit_code -eq 2 || $exit_code -eq 130 ]]; then
     echo "helm_apply_required=true" >> "$GITHUB_OUTPUT"
     echo "Changes dectected in helm diff"
 
-    ./helmfile diff --environment lower -f $1 --detailed-exitcode &>> loki_helm_diff_summary
+    ./helmfile diff --environment lower -f $app --detailed-exitcode &>> loki_helm_diff_summary
     echo "diff-summary=$(cat loki_helm_diff_summary)" >> $GITHUB_STEP_SUMMARY
 
     echo "Building helm .... "
-    ./helmfile build --environment lower -f $1
+    ./helmfile build --environment lower -f $app
 
     echo "Apply helm .... "
-    ./helmfile apply --environment lower -f $1
+    ./helmfile apply --environment lower -f $app
     
     exit 0
 elif [[ $exit_code -eq 1 ]]; then
     echo "Helm diff resulted in error with exit code 1"
 
-    ./helmfile diff --environment lower -f $1 --detailed-exitcode &>> loki_helm_diff_summary
+    ./helmfile diff --environment lower -f $app --detailed-exitcode &>> loki_helm_diff_summary
     echo "diff-summary=$(cat loki_helm_diff_summary)" >> $GITHUB_STEP_SUMMARY
 
     exit 1
 else
     echo "No changes detected in helm diff"
 
-    ./helmfile diff --environment lower -f $1 --detailed-exitcode &>> loki_helm_diff_summary
+    ./helmfile diff --environment lower -f $app --detailed-exitcode &>> loki_helm_diff_summary
     echo "diff-summary=$(cat loki_helm_diff_summary)" >> $GITHUB_STEP_SUMMARY
 
     exit 0
